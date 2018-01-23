1f0b2541e190754f8d1391af7efc6a24a0e33a12
Алексей Коломенцов <amkolomentsov@edu.hse.ru>
2018-01-23 23:06:31 +0300

    1.1

--- UserInterface/EmptyTerminals.xaml.cs
+++ UserInterface/EmptyTerminals.xaml.cs
@@ -12,7 +12,6 @@ namespace UserInterface
     {
        public EmptyTerminals()
         {
-
             InitializeComponent();
             DataGrid.ItemsSource = new Helper().GetTerminalsWithEmptyItems();
         }
